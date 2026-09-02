1ee6892ae8dd30b99eb96ce9ef472bb42dea48f9
LeonardoSilva1203 <leonardosilva120308@gmail.com>
2026-09-02 14:35:25 -0300

    feat: Importando todas as páginas no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Integrantes from './pages/Integrantes';
-import Sobre from './pages/Sobre';
-import Faq from './pages/Faq';
-import Contato from './pages/Contato';
-import Solucao from './pages/Solucao';
-import Carteira from './pages/Carteira';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Integrantes from "./pages/Integrantes";
+import Sobre from "./pages/Sobre";
+import Faq from "./pages/Faq";
+import Contato from "./pages/Contato";
+import Solucao from "./pages/Solucao";
+import Carteira from "./pages/Carteira";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}><Route path="/home" element={<Home />} />
         <Route path="/integrantes" element={<Integrantes />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/solucao" element={<Solucao />} />
-        <Route path="/Carteira" element={<Carteira />} />
+        <Route path="/carteira" element={<Carteira />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
